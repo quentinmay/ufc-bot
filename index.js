ac3bb4a9813f5140251718c18623d75477a63a66
quentinmay <quentinemay@gmail.com>
2021-08-18 18:40:07 -0700

    Added more commands. give, take. other small fixes

--- index.js
+++ index.js
@@ -52,6 +52,51 @@ async function getGuildMemberFromServerIDAndUserID(serverID, id) {
     return;
 
 }
+async function displayMatches(textChannel, contents) {
+    if (contents) {
+        let fightID = contents;
+        let fight = await ufc.getFight(fightID);
+        if (fight) {
+            textChannel.send("`" + `${fight.event_id} - ${fight.away_name}(${fight.away_odds}) vs. ${fight.home_name}(${fight.home_odds})` + "`");
+        } else {
+            textChannel.send("`This fight doesn't exist.`");
+        }
+        return true;
+    } else {//No user mentioned. Just display all matches.
+        const matchesEmbed = {
+            title: `__Matches__`,
+            url: `https://www.oddsshark.com/ufc`,
+            color: 10181046, //this is purple in their weird color system thing https://leovoel.github.io/embed-visualizer/
+            footer: {
+                icon_url: client.user.displayAvatarURL(),
+                text: `Matches listed.`
+            },
+            author: {
+                name: "Matches",
+                url: "",
+                icon_url: client.user.displayAvatarURL()
+            },
+            fields: []
+        }
+        let i = 0;
+
+        for (let upcomingMatch of ufc.upComingMatches) {
+            matchesEmbed.fields.push({
+                name: `${upcomingMatch.event_id} - Fighters: ${upcomingMatch.away_name} vs. ${upcomingMatch.home_name}`,
+                value: `Odds: ${upcomingMatch.away_odds} vs. ${upcomingMatch.home_odds}`,
+                url: "",
+            })
+            i++;
+            if (i > 24) break;
+        }
+        textChannel.send({
+            embed: matchesEmbed
+        });
+        return true;
+    }
+
+
+}
 
 async function displayBets(textChannel, mentions) {
     const betsEmbed = {
@@ -104,7 +149,7 @@ async function displayUser(textChannel, mentions, author) {
         ufcUser = await ufc.findUser(author.id);
     }
     if (ufcUser) {
-        textChannel.send("`" + `Balance: ${ufcUser.balance}, # of Current Bets: ${ufcUser.currentBets.length}` + "`")
+        textChannel.send("`" + `Balance: $${ufcUser.balance}, Current Bets: ${ufcUser.currentBets.length}` + "`")
     } else {
         textChannel.send("`User doesn't have an account.`");
     }
@@ -146,6 +191,33 @@ async function getUserBets() {
 
 }
 
+function isAdmin(userID) {
+    return userID == config.discordDevID;
+
+}
+
+async function giveMoney(mentions, content) {
+    let cash = content.split(" ")[1];
+    if (!isNaN(cash)) {
+        if (mentions.users.size == 1) {
+            let targetUser = mentions.users.first().id;
+            return (await ufc.addMoney(targetUser, cash));
+        }
+    }
+    return false;
+}
+async function takeMoney(mentions, content) {
+    let cash = content.split(" ")[1];
+    if (!isNaN(cash)) {
+        if (mentions.users.size == 1) {
+            let targetUser = mentions.users.first().id;
+            return (await ufc.takeMoney(targetUser, cash));
+        }
+    }
+    return false;
+}
+
+
 
 
 client.on('message', async (msg) => {
@@ -157,7 +229,7 @@ client.on('message', async (msg) => {
         let tmp = rawString.split(" ");
         tmp.shift();
         let contents = tmp.join(" ");
-        //TODO adding more commands. Start with matches (copied from bets), create checkAdmin, then give, take, bet, 1v1, then lastly refresh. 
+        //TODO adding more commands. Start with create bet, 1v1, then lastly refresh. 
         switch (cmd) {
             //Display all valid bets. If @ member then display only that members valid bets.
             case 'bets':
@@ -166,6 +238,8 @@ client.on('message', async (msg) => {
             //Display all matches and their betting odds.
             case 'matches':
             case 'match':
+            case 'upcomingmatches':
+                displayMatches(textChannel, contents);
                 break;
             //Display user details
             case 'user':
@@ -194,9 +268,33 @@ client.on('message', async (msg) => {
             //Admin Commands:
             //Gives money to user @ mentioned.
             case 'give':
+            case 'add':
+            case 'addmoney':
+            case 'givemoney':
+                if (isAdmin(msg.author.id)) {
+                    if (await giveMoney(msg.mentions, contents)) {
+                        textChannel.send("`" + `Gave user ${contents.split(" ")[1]}.` + "`")
+                    } else {
+                        textChannel.send("`" + `Failed to give user money.` + "`")
+                    }
+                } else {
+                    textChannel.send("`You don't have permission for that command.`");
+                }
                 break;
             // Takes money from user @ mentioned.
             case 'take':
+            case 'remove':
+            case 'takemoney':
+            case 'removemoney':
+                if (isAdmin(msg.author.id)) {
+                    if (await takeMoney(msg.mentions, contents)) {
+                        textChannel.send("`" + `Took ${contents.split(" ")[1]} from user.` + "`")
+                    } else {
+                        textChannel.send("`" + `Failed to take users money.` + "`")
+                    }
+                } else {
+                    textChannel.send("`You don't have permission for that command.`");
+                }
                 break;
             //Refreshes match data retrieved from oddsharks.
             case 'refresh':
